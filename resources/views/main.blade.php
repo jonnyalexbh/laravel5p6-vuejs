74e297e556e7662be29b0dfce0f75686a8339444
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-05 15:14:37 -0500

    Laravel Mix, compiling CSS files, JavaScript

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,11 +10,8 @@
   <meta name="keyword" content="Sales system Laravel Vue Js, Shopping system Laravel Vue Js">
   <link rel="shortcut icon" href="img/favicon.png">
   <title>Sales System - jonnyalexbh</title>
-  <!-- Icons -->
-  <link href="css/font-awesome.min.css" rel="stylesheet">
-  <link href="css/simple-line-icons.min.css" rel="stylesheet">
-  <!-- Main styles for this application -->
-  <link href="css/style.css" rel="stylesheet">
+  <!-- Template -->
+  <link href="css/template.css" rel="stylesheet">
 </head>
 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed aside-menu-hidden">
@@ -156,14 +153,7 @@
     <span class="ml-auto">Desarrollado por <a href="https://github.com/jonnyalexbh">jonnyalexbh</a></span>
   </footer>
 
-  <!-- Bootstrap and necessary plugins -->
-  <script src="js/jquery.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/pace.min.js"></script>
-  <!-- Plugins and scripts required by all views -->
-  <script src="js/Chart.min.js"></script>
-  <!-- GenesisUI main scripts -->
+  <!-- Template -->
   <script src="js/template.js"></script>
 </body>
 
