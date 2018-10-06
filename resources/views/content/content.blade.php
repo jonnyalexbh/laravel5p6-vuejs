0cc880ef4215a34e8679e4ac2c903b06176b10e5
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-05 20:40:03 -0500

    configuring the menu with VueJs

--- resources/views/content/content.blade.php
+++ resources/views/content/content.blade.php
@@ -1,4 +1,54 @@
 @extends('main')
 @section('content')
-  <example-component></example-component>
+  <template v-if="menu==0">
+    <example-component></example-component>
+  </template>
+
+  <template v-if="menu==1">
+    <h3>content of the menu 1</h3>
+  </template>
+
+  <template v-if="menu==2">
+    <h3>content of the menu 2</h3>
+  </template>
+
+  <template v-if="menu==3">
+    <h3>content of the menu 3</h3>
+  </template>
+
+  <template v-if="menu==4">
+    <h3>content of the menu 4</h3>
+  </template>
+
+  <template v-if="menu==5">
+    <h3>content of the menu 5</h3>
+  </template>
+
+  <template v-if="menu==6">
+    <h3>content of the menu 6</h3>
+  </template>
+
+  <template v-if="menu==7">
+    <h3>content of the menu 7</h3>
+  </template>
+
+  <template v-if="menu==8">
+    <h3>content of the menu 8</h3>
+  </template>
+
+  <template v-if="menu==9">
+    <h3>content of the menu 9</h3>
+  </template>
+
+  <template v-if="menu==10">
+    <h3>content of the menu 10</h3>
+  </template>
+
+  <template v-if="menu==11">
+    <h3>content of the menu 11</h3>
+  </template>
+
+  <template v-if="menu==12">
+    <h3>content of the menu 12</h3>
+  </template>
 @endsection
