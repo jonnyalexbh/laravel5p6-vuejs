7977b4c2e2e786ed1db64e0469006c6be57e7530
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-26 23:34:18 -0500

    migration articles

---
+++ database/migrations/2018_10_27_042836_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('code', 50)->nullable();
+            $table->string('name', 100)->unique();
+            $table->decimal('sale_price', 11, 2);
+            $table->integer('stock');
+            $table->string('description', 256)->nullable();
+            $table->boolean('condition')->default(1);
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
